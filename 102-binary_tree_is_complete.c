fff70ca989062f72aeabd3cb27da2ffcb17f8103
Red-Rim <salmaouakhir20@gmail.com>
2023-11-02 21:14:30 +0100

    binary_tree_complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,36 +1,46 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_complete - Check if a binary tree is complete.
+ * binary_tree_is_complete - Checks if a binary tree is complete.
  * @tree: A pointer to the root node of the tree to check.
- * Return: 1 if complete, 0 otherwise.
+ * Return: 0 if tree is NULL, 1 if it's complete, 0 otherwise.
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    int is_leaf_found = 0;
-    binary_tree_t *queue[1024];  /* Assuming a maximum of 1024 nodes */
-    int front = 0, rear = 0;
-
     if (tree == NULL)
         return (0);
 
+    int is_leaf_found = 0;
+    binary_tree_t *queue[1024];
+    size_t front = 0, rear = 0;
+
     queue[rear++] = (binary_tree_t *)tree;
 
-    while (front < rear)
+    while (rear != front)
     {
-        binary_tree_t *current = queue[front++;
-
-        if (current == NULL)
+        binary_tree_t *current = queue[front++];
+        
+        if (current->left)
+        {
+            if (is_leaf_found)
+                return (0);
+            queue[rear++] = current->left;
+        }
+        else
         {
             is_leaf_found = 1;
         }
-        else
+
+        if (current->right)
         {
             if (is_leaf_found)
                 return (0);
 
-            queue[rear++] = current->left;
             queue[rear++] = current->right;
+        }
+        else
+        {
+            is_leaf_found = 1;
         }
     }
 
